a08a4dd5a72cd2dc9eb6945a5d58b5d398a5b8b6
Evan Moran <moraneva@msu.edu>
2019-06-26 14:44:19 -0400

    Working

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,9 +2,37 @@ pragma solidity ^0.5.0;
 
 contract Election {
 
-    string public candidate;
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+    mapping(address => bool) public voters;
+
+    uint public candidatesCount;
+
+    event votedEvent (
+        uint indexed _candidateId
+    );
 
     constructor() public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _candidateId) public {
+        require(!voters[msg.sender], "Already voted");
+        require(_candidateId > 0 && _candidateId <= candidatesCount, "Invalid candidate id");
+
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount++;
+        emit votedEvent(_candidateId);
     }
 }
